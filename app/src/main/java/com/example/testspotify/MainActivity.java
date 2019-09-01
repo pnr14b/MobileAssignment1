0c9acddd3ad7d730cbfe857b2db93ec4890b9547
pnr14b <pnr14b@my.fsu.edu>
2019-09-01 15:17:35 -0400

    firebase functionality

--- app/src/main/java/com/example/testspotify/MainActivity.java
+++ app/src/main/java/com/example/testspotify/MainActivity.java
@@ -14,9 +14,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.testspotify.ui.login.LoginActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.JsonObject;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -25,6 +29,8 @@ import com.spotify.protocol.types.Track;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import org.json.JSONObject;
 
 import java.util.Base64;
 import java.util.HashMap;
@@ -37,10 +43,14 @@ public class MainActivity<stringRequest> extends AppCompatActivity {
     private static final String CLIENT_ID = "783249b827704cbdab0b62f069fe51c4";
     private static final String REDIRECT_URI = "youcustomprotocol://callback";
     public SpotifyAppRemote mSpotifyAppRemote;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance("https://testspotify-42d61.firebaseio.com/");
+        //database.getReference("hello").setValue("this is a test");
     }
 
 
@@ -54,7 +64,7 @@ public class MainActivity<stringRequest> extends AppCompatActivity {
         AuthenticationRequest.Builder builder =
                 new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
 
-        builder.setScopes(new String[]{"user-top-read"});
+        builder.setScopes(new String[]{"user-top-read","user-library-read","user-read-recently-played","user-modify-playback-state"});
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
@@ -72,44 +82,38 @@ public class MainActivity<stringRequest> extends AppCompatActivity {
     // Instantiate the RequestQueue.
     RequestQueue queue = Volley.newRequestQueue(this);
 
-    String url = "https://api.spotify.com/v1/me/top/tracks";
-    //String url = "https://www.google.com";
+    String url = "https://api.spotify.com/v1/me/player/recently-played";
 
-        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        // response
-                        Log.d("Success",response);
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        Log.d("ERROR","error => "+error.toString());
-                    }
-                }
-        )
-        {
+        Map<String, String> params = new HashMap();
+        params.put("first_param", "5");
+
+
+        JSONObject parameters = new JSONObject(params);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url,parameters,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("onResponse", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("onErrorResponse", error.toString());
+            }
+        }) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String>  params = new HashMap<String, String>();
-                params.put("Content-Type", "application/json");
-                String auth = "Bearer " + tok;
-                        //+ Base64.getEncoder().encodeToString(tok.getBytes());
-                params.put("Authorization", auth);
+                Map<String, String> headers = new HashMap<>();
 
 
-
-                return params;
+                headers.put("Authorization", "Bearer " + tok);
+                return headers;
             }
         };
-
+        queue.add(request);
     // Add the request to the RequestQueue.
-    queue.add(getRequest);
+    queue.add(request);
 
 }
 
